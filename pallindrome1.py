6628d8140d8da97b1fde604cd90407df552a7747
Tamil vanan <50619196+tamilvn@users.noreply.github.com>
2019-05-15 11:40:24 +0530

    Update pallindrome1.py

--- pallindrome1.py
+++ pallindrome1.py
@@ -1,10 +1,10 @@
-val=int(input())
+val2=int(input())
 sum=0
-temp=val
-while(val>0):
-    dig=val%10
+temp=val2
+while(val2>0):
+    dig=val2%10
     sum=sum*10+dig
-    val=val//10
+    val2=val2//10
 if(temp==sum):
     print("yes")
 else:
