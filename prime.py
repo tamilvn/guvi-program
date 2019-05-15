95f95557548c3fbb2ff23c8add21349b661ded37
Tamil vanan <50619196+tamilvn@users.noreply.github.com>
2019-05-15 11:31:18 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,7 +1,7 @@
-val1=int(input())
-if(val1>1):
-    for i in range(2,val1):
-        if(val1%i==0):
+val2=int(input())
+if(val2>1):
+    for i in range(2,val2):
+        if(val2%i==0):
             print("no")
             break
         else:
