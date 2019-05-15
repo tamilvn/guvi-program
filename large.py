21ba6c4c048a2a870097585564a85eebe6754eac
Tamil vanan <50619196+tamilvn@users.noreply.github.com>
2019-05-15 09:09:44 +0530

    Create large.py

---
+++ large.py
@@ -0,0 +1,9 @@
+val1=int(input("first value"))
+val2=int(input("second value"))
+val3=int(input("third value"))
+if(val1>val2 and val1>val3):
+    print(val1)
+elif(val2>val1 and val2>val3):
+    print(val2)
+else:
+    print(val3)
