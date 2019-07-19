12c81302a6b9adcebecd99dd7415fec471df7efd
Tamil vanan <50619196+tamilvn@users.noreply.github.com>
2019-07-19 12:21:23 +0530

    Create arithprog.py

---
+++ arithprog.py
@@ -0,0 +1,7 @@
+a = int(input("Please Enter First Number of an A.P Series: : "))
+n = int(input("Please Enter the Total Numbers in this A.P Series: : "))
+d = int(input("Please Enter the Common Difference : "))
+
+total = (n * (2 * a + (n - 1) * d)) / 2
+
+print("\nThe Sum of Arithmetic Progression Series = " , total)
